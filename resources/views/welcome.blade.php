294c3a759d522db00ae4fc91daebb5e5aa0f9bf1
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-05-29 20:01:53 +0200

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,49 +1,5 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('layout')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        @unless (empty($people))
-            <h3>There are some people</h3>
-        @else
-            Nobody's here yo
-        @endif
-        
-        @foreach ($people as $person)
-            <li>{{ $person }}</li>
-        @endforeach
-    </body>
-</html>
+@section('content')
+    <h1>The welcome page goes here</h1>
+@stop
