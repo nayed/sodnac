7ef384b6119a4a3852b6fb26867e375d709a47cf
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-08-10 23:52:35 +0200

    playing around

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,6 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('style.sass')
+        .version('css/style.css')
 });
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Document</title>
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{ elixir('css/style.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
