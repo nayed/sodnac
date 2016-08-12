123aebbe71f630b963f56f73cf4e49e339c52a29
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-08-12 20:18:58 +0200

    add notes to a card

---
+++ app/Http/Controllers/NotesController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Note;
+use App\Card;
+use Illuminate\Http\Request;
+
+class NotesController extends Controller
+{
+    public function store(Request $request, Card $card)
+    {
+        $card->addNote(
+            new Note($request->all())
+        );
+
+        return back();
+    }
+}
